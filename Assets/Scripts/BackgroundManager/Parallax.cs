fdd0967abaeb74ea93deabdd93335fec0fd6ff0d
takoz53 <takoz53@users.noreply.github.com>
2022-10-22 19:30:30 +0200

    Create Background Obj, Parallax Obj and Parallax Script

---
+++ Assets/Scripts/BackgroundManager/Parallax.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [Header("Camera Settings")]
+    public float cameraMoveSpeed = 1.5f;
+    [Header("Layer Settings")]
+    public float[] layerSpeed = new float[5];
+    public GameObject[] layerObjects = new GameObject[5];
+
+    private Transform mainCamera;
+    private float[] startPos = new float[5];
+    private float boundsSizeX;
+    private float localScaleX;
+    private int layerSize;
+    /// <summary>
+    /// Setting up the layers
+    /// </summary>
+    void Start()
+    {
+        layerSize = layerSpeed.Length;
+        mainCamera = Camera.main.transform;
+        localScaleX = layerObjects[0].transform.localScale.x;
+        boundsSizeX = layerObjects[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        for (int i=0; i < layerSize; i++) {
+            startPos[i] = mainCamera.position.x;
+        }
+    }
+    
+    /// <summary>
+    /// Update Method, which moves the main Camera slightly and moves the other layer objects relative to the camera.
+    /// </summary>
+    void Update() {
+        mainCamera.position += Vector3.right * Time.deltaTime * cameraMoveSpeed;
+        for (int i=0; i < layerSize; i++) {
+            float temp = mainCamera.position.x * (1 - layerSpeed[i]);
+            float distance = mainCamera.position.x  * layerSpeed[i];
+            layerObjects[i].transform.position = new Vector2 (startPos[i] + distance, mainCamera.position.y);
+            if (temp > startPos[i] + boundsSizeX * localScaleX) {
+                startPos[i] += boundsSizeX * localScaleX;
+            } else if(temp < startPos[i] - boundsSizeX * localScaleX) {
+                startPos[i] -= boundsSizeX * localScaleX;
+            }
+        }
+    }
+}
