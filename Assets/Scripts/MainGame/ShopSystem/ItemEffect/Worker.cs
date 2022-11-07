10533724672d69d1b7052082e51fe3df059a44e7
Patrick Voca <pvoca@stud.hs-heilbronn.de>
2022-11-07 14:13:23 +0100

    trying to implement worker into shop

---
+++ Assets/Scripts/MainGame/ShopSystem/ItemEffect/Worker.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+using UnityEngine.UI;
+
+class Worker : ItemEffect
+{
+    public new string id = "Worker";
+    public ShopItem shopItem;
+    public Button purchaseButton;
+    private bool hasUpdated = false;
+    private int creditsPerSec;
+    private int workerAmount = 0;
+    private float timer = 0.0f;
+    private int credits;
+
+
+
+
+
+
+    public Worker(ShopManager manager) : base(manager)
+    {
+        base.shopManager = manager;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        creditsPerSec = workerAmount * 5;
+        
+        credits = creditsPerSec;
+        shopManager.dummyButtonObj.credits =+credits;
+        Debug.Log(credits);
+
+
+
+
+    }
+
+    public override void PurchaseButtonAction() {
+        workerAmount++;
+        
+
+    }
+
+    public override int CalculateNewPrice(ShopItem shopItem)
+    {
+        return shopItem.price *= 2;
+    }
+
+
+
+}
