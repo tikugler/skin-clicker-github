da8e8760968cd6731669da8e090df046393b8965
yberber <yusufberber19@gmail.com>
2022-11-09 21:55:55 +0100

    added documentation to DatabaseManager Script

--- Assets/Scripts/DatabaseManager.cs
+++ Assets/Scripts/DatabaseManager.cs
@@ -8,6 +8,8 @@ public class DatabaseManager : MonoBehaviour
 
     private static string serverAddress = "https://skinclicker.000webhostapp.com";
 
+    // registers user into DB by using username, password and email
+    // username and password should be unique, otherwise it returns error
     public static IEnumerator Register(string username, string password, string email)
     {
         WWWForm form = new WWWForm();
@@ -22,7 +24,7 @@ public class DatabaseManager : MonoBehaviour
     }
 
 
-
+    // check if user exists in db
     public static IEnumerator LoginPlayer( string username, string password)
     {
         WWWForm form = new WWWForm();
@@ -35,7 +37,7 @@ public class DatabaseManager : MonoBehaviour
         yield return result;
     }
 
-
+    // updates score by given username
     public static IEnumerator SavePlayerData(string serverAddress, string username, int score)
     {
         WWWForm form = new WWWForm();
