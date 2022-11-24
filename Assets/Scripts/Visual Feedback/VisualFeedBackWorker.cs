0a664de9c48b81a9bba7f148d2d6cef3f0cf5f48
Patrick Voca <pvoca@stud.hs-heilbronn.de>
2022-11-24 20:09:12 +0100

    implemented logic for Visual Feedback Worker

--- Assets/Scripts/Visual Feedback/VisualFeedBackWorker.cs
+++ Assets/Scripts/Visual Feedback/VisualFeedBackWorker.cs
@@ -1,35 +1,35 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class VisualFeedBackWorker : MonoBehaviour
 {
-    public Texture2D tex;
-    private Sprite pickaxe;
-    private SpriteRenderer sr;
-    
-    void Awake()
-    {
-        //sr = gameObject.AddComponent<SpriteRenderer>() as SpriteRenderer;
-        
+    public GameObject pickaxePrefab;
+    public GameObject pickaxe;
 
-        //transform.position = new Vector3(1.5f, 1.5f, 0.0f);
-    }
+    public float posX = 800f;
+    public float posY = 450f;
 
-    void Start()
+    //creates and shows a worker on screen in a row  every time after buying 5 worker 
+    public void MultipleWorker(int workerCount)
     {
-        //pickaxe = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f), 100.0f);
-    }
-
-    public void  MultipleWorker(int workerCount)
-    {
-        if ((workerCount % 5) == 0)
+        if ((workerCount % 5) == 0 && posX < 1070)
         {
             //sr.sprite = pickaxe;
+            pickaxePrefab = Resources.Load("pickaxe") as GameObject;
+            pickaxe = GameObject.Instantiate(pickaxePrefab, new Vector2(posX += 45f, posY), transform.rotation);
             Debug.Log("5 Worker bought" + workerCount);
         }
-    }
-
-   
+        else if ((workerCount % 5) == 0 && posX >= 1070)
+        {
+            posX = 800f;
+            pickaxePrefab = Resources.Load("pickaxe") as GameObject;
+            pickaxe = GameObject.Instantiate(pickaxePrefab, new Vector2(posX += 45, posY -= 45f), transform.rotation);
+            Debug.Log("5 Worker bought" + workerCount);
+        }
+        if (posY <= 360)
+            Destroy(pickaxe);
+        }
 }
 
