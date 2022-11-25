c1843657c4612dfb643c57767d35a40b272e5162
Timo Kugler <tikugler@stud.hs-heilbronn.de>
2022-11-25 18:36:14 +0100

    Adding test items to shop test

--- Assets/Tests/PlayMode/Shop/TestShopManager.cs
+++ Assets/Tests/PlayMode/Shop/TestShopManager.cs
@@ -12,8 +12,49 @@ public class ShopManagerTest
     private GameObject canvasGameObject;
     private ContentDistributor distributor;
 
-    public ItemTemplate[] scriptableObjectItemsTest = new ItemTemplate[1];
     private ItemTemplate itemTemplate1;
+    private string id1 = "DoubleEffect";
+    private string title1 = "Double Effect 1";
+    private string description1 = "Test description double effect 1";
+    private int amount1 = 0;
+    private int price1 = 333;
+    private Sprite icon1 = null;
+
+    private ItemTemplate itemTemplate2;
+    private string id2 = "Worker";
+    private string title2 = "Worker 1";
+    private string description2 = "Test description Worker 1";
+    private int amount2 = 0;
+    private int price2 = 4;
+    private Sprite icon2 = null;
+
+private ItemTemplate itemTemplate3;
+    private string id3 = "Worker";
+    private string title3 = "Deutsche Bahn";
+    private string description3 = "Guess who's late";
+    private int amount3 = 3;
+    private int price3 = 90;
+    private Sprite icon3 = null;
+
+    private ItemTemplate itemTemplate4 ;
+    private string id4 = "DoubleEffect";
+    private string title4 = "rng text";
+    private string description4 = "Too tired for a creative text";
+    private int amount4 = 7;
+    private int price4 = 921;
+    private Sprite icon4 = null;
+
+    private ItemTemplate itemTemplate5;
+    private string id5 = "DoubleEffect";
+    private string title5 = "Free Item";
+    private string description5 = "Free and useless effect ;)";
+    private int amount5 = 78;
+    private int price5 = 0;
+    private Sprite icon5 = null;
+
+    public ItemTemplate[] scriptableObjectItemsTestOneItem = new ItemTemplate[1];
+    public ItemTemplate[] scriptableObjectItemsTestTwoItems = new ItemTemplate[2];
+    public ItemTemplate[] scriptableObjectItemsTestFiveItems = new ItemTemplate[5];
 
 
     [OneTimeSetUp]
@@ -39,18 +80,57 @@ public class ShopManagerTest
     private void CreateItemTemplateForTesting()
     {
         //Create new testItem and add to scriptableObjectItemsTest
-        string description = "Test description";
-        string id = "DoubleEffect";
-        string title = "TestItemV1";
-        int price = 333;
-
         itemTemplate1 = ScriptableObject.CreateInstance("ItemTemplate") as ItemTemplate;
-        itemTemplate1.amount = 0;
-        itemTemplate1.description = description;
-        itemTemplate1.icon = null;
-        itemTemplate1.id = id;
-        itemTemplate1.price = price;
-        itemTemplate1.title = title;
+        itemTemplate1.amount = amount1;
+        itemTemplate1.description = description1;
+        itemTemplate1.icon = icon1;
+        itemTemplate1.id = id1;
+        itemTemplate1.price = price1;
+        itemTemplate1.title = title1;
+
+        itemTemplate2 = ScriptableObject.CreateInstance("ItemTemplate") as ItemTemplate;
+        itemTemplate2.amount = amount2;
+        itemTemplate2.description = description2;
+        itemTemplate2.icon = icon2;
+        itemTemplate2.id = id2;
+        itemTemplate2.price = price2;
+        itemTemplate2.title = title2;
+
+        itemTemplate3 = ScriptableObject.CreateInstance("ItemTemplate") as ItemTemplate;
+        itemTemplate3.amount = amount3;
+        itemTemplate3.description = description3;
+        itemTemplate3.icon = icon3;
+        itemTemplate3.id = id3;
+        itemTemplate3.price = price3;
+        itemTemplate3.title = title3;
+
+        itemTemplate4 = ScriptableObject.CreateInstance("ItemTemplate") as ItemTemplate;
+        itemTemplate4.amount = amount4;
+        itemTemplate4.description = description4;
+        itemTemplate4.icon = icon4;
+        itemTemplate4.id = id4;
+        itemTemplate4.price = price4;
+        itemTemplate4.title = title4;
+
+        itemTemplate5 = ScriptableObject.CreateInstance("ItemTemplate") as ItemTemplate;
+        itemTemplate5.amount = amount5;
+        itemTemplate5.description = description5;
+        itemTemplate5.icon = icon5;
+        itemTemplate5.id = id5;
+        itemTemplate5.price = price5;
+        itemTemplate5.title = title5;
+
+        scriptableObjectItemsTestOneItem[0] = itemTemplate1;
+
+        scriptableObjectItemsTestTwoItems[0] = itemTemplate1;
+        scriptableObjectItemsTestTwoItems[1] = itemTemplate2;
+
+        scriptableObjectItemsTestFiveItems[0] = itemTemplate1;
+        scriptableObjectItemsTestFiveItems[1] = itemTemplate2;
+        scriptableObjectItemsTestFiveItems[2] = itemTemplate3;
+        scriptableObjectItemsTestFiveItems[3] = itemTemplate4;
+        scriptableObjectItemsTestFiveItems[4] = itemTemplate5;
+
     }
 
     private void OpenShopPopUpWithButton()
@@ -81,11 +161,10 @@ public class ShopManagerTest
         yield return null;
         //Array.Clear(distributor.scriptableObjectItems, 0, distributor.scriptableObjectItems.Length);
 
-        Debug.Log("Size of new SO-List before adding: " + scriptableObjectItemsTest.Length);
+        Debug.Log("Size of new SO-List before adding: " + scriptableObjectItemsTestOneItem.Length);
         Debug.Log("Dead");
-        scriptableObjectItemsTest[0] = itemTemplate1;
-        distributor.scriptableObjectItems = scriptableObjectItemsTest;
-        Debug.Log("Size of new SO-List after adding: " + scriptableObjectItemsTest.Length);
+        distributor.scriptableObjectItems = scriptableObjectItemsTestOneItem;
+        Debug.Log("Size of new SO-List after adding: " + scriptableObjectItemsTestOneItem.Length);
         int counterForActivePanels = 0;
         manager.RefreshPanels();
         yield return null;
