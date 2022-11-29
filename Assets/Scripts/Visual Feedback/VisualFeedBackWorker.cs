6d358b9b095675aa7f591d9dbab56dbc3b568257
Patrick Voca <pvoca@stud.hs-heilbronn.de>
2022-11-29 19:29:10 +0100

    rewrote code completly, added Scrollview for pickaxes and removed pickaxe tag

--- Assets/Scripts/Visual Feedback/VisualFeedBackWorker.cs
+++ Assets/Scripts/Visual Feedback/VisualFeedBackWorker.cs
@@ -2,34 +2,27 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using static UnityEditor.Progress;
 
 public class VisualFeedBackWorker : MonoBehaviour
-{
+{   
     public GameObject pickaxePrefab;
     public GameObject pickaxe;
 
-    public float posX = 800f;
-    public float posY = 450f;
-
-    //creates and shows a worker on screen in a row  every time after buying 5 worker 
+    //creates and showss a worker on screen every time after buying 5 worker 
     public void MultipleWorker(int workerCount)
     {
-        if ((workerCount % 5) == 0 && posX < 1070)
+        if ((workerCount % 5) == 0)
         {
-            //sr.sprite = pickaxe;
-            pickaxePrefab = Resources.Load("pickaxe") as GameObject;
-            pickaxe = GameObject.Instantiate(pickaxePrefab, new Vector2(posX += 45f, posY), transform.rotation);
+            pickaxePrefab = Resources.Load("pickaxePrefab") as GameObject;
+            pickaxe = GameObject.Instantiate(pickaxePrefab);
+            pickaxe.transform.parent = GameObject.Find("pickaxescroll").transform;
             Debug.Log("5 Worker bought" + workerCount);
         }
-        else if ((workerCount % 5) == 0 && posX >= 1070)
-        {
-            posX = 800f;
-            pickaxePrefab = Resources.Load("pickaxe") as GameObject;
-            pickaxe = GameObject.Instantiate(pickaxePrefab, new Vector2(posX += 45, posY -= 45f), transform.rotation);
-            Debug.Log("5 Worker bought" + workerCount);
-        }
-        if (posY <= 360)
-            Destroy(pickaxe);
+        
         }
 }
 
