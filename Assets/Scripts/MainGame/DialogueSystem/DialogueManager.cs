c54f5df8596c35410801aac9a2ddbfe9ca380e61
takoz53 <takoz53@users.noreply.github.com>
2022-11-30 23:05:03 +0100

    Add Dialogue System

---
+++ Assets/Scripts/MainGame/DialogueSystem/DialogueManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+
+	public TextMeshProUGUI nameText;
+	public TextMeshProUGUI dialogueText;
+
+	public Animator animator;
+
+	private Queue<string> sentences;
+
+    // Use this for initialization
+    void Start()
+	{
+		sentences = new Queue<string>();
+	}
+
+	public void StartDialogue(Dialogue dialogue)
+	{
+		animator.transform.gameObject.SetActive(true);
+		animator.SetBool("IsOpen", true);
+		
+		nameText.text = dialogue.name;
+
+		sentences.Clear();
+
+		foreach (string sentence in dialogue.sentences)
+		{
+			sentences.Enqueue(sentence);
+		}
+		DisplayNextSentence();
+	}
+
+	public void DisplayNextSentence()
+	{
+		if (sentences.Count == 0)
+		{
+			EndDialogue();
+			return;
+		}
+
+		string sentence = sentences.Dequeue();
+		Debug.Log(sentence);
+		StopAllCoroutines();
+		StartCoroutine(TypeSentence(sentence));
+	}
+
+	IEnumerator TypeSentence(string sentence)
+	{
+		dialogueText.text = "";
+		foreach (char letter in sentence.ToCharArray())
+		{
+			dialogueText.text += letter;
+			yield return null;
+		}
+	}
+
+	void EndDialogue()
+	{
+		animator.SetBool("IsOpen", false);
+	}
+
+}
---
+++ Assets/Scripts/MainGame/DialogueSystem/DialogueTrigger.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+
+	public Dialogue dialogue;
+
+	public void TriggerDialogue()
+	{
+		FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+	}
+
+}
