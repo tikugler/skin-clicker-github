5d4423b6bb204bb62e333fc9609f64f81bfa41ab
Janick Thimig <janick.thimig@gmx.de>
2022-11-30 22:24:30 +0100

    Creditanzeige im MainGame

---
+++ Assets/Scripts/MainGame/ScoreManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+   
+    public Text creditUIText;
+    public static int credit = Account.credits;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        RefreshCredits();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RefreshCredits();
+    }
+
+    private void RefreshCredits()
+    {
+        
+        credit = Account.credits;
+        creditUIText.text = "$ " + credit.ToString();
+    }
+
+}
