fe0acb1ded7fc9affce6f04ef21542161610073a
Janick Thimig <janick.thimig@gmx.de>
2022-11-30 23:21:04 +0100

    Mainbutton bigger and every 50 clicks 400 more points.

--- Assets/Scripts/MainGame/DummyButton.cs
+++ Assets/Scripts/MainGame/DummyButton.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 public class DummyButton : MonoBehaviour
 {
     public int multiplicator = 1;
-    public int basePoints = 1; 
+    public int basePoints = 1;
+    private int xClickBonusCounter = 0;
    
 
     public void MainButtonAction() 
     {
         Account.credits += basePoints * multiplicator;
+        xClickBonusCounter++;
+        if(xClickBonusCounter == 50)
+        {
+            Account.credits += 400;
+            xClickBonusCounter = 0;
+        }
     }
 
 
