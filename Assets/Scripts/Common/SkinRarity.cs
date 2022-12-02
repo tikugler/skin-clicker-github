233a33ee865449937c65c66070223355b932ca41
Timo Kugler <tikugler@stud.hs-heilbronn.de>
2022-12-02 11:07:39 +0100

    Adding SkinRarity

---
+++ Assets/Scripts/Common/SkinRarity.cs
@@ -0,0 +1,8 @@
+public static class SkinRarity
+{
+    public static readonly string Common = "Common";
+    public static readonly string Uncommon = "Uncommon";
+    public static readonly string Rare = "Rare";
+    public static readonly string Mythical = "Mythical";
+    public static readonly string Legendary = "Legendary";
+}
