735c9c862ef4f046bc0ff0ea60deccb8e1d1d16c
yberber <yusufberber19@gmail.com>
2022-12-04 01:08:41 +0100

    implement friend list

---
+++ Assets/Scripts/PlayFabFriendManager.cs
@@ -0,0 +1,170 @@
+using System.Collections;
+using System.Collections.Generic;
+using PlayFab;
+using PlayFab.ClientModels;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayFabFriendManager : MonoBehaviour
+{
+
+    enum FriendIdType { PlayFabId, Username, Email, DisplayName };
+
+    //List<FriendInfo> _friends = null;
+
+    [SerializeField] Transform friendScrollView;
+    public GameObject uiFriendPrefab;
+    List<FriendInfo> myFriendsInScrollView = new List<FriendInfo>();
+    [SerializeField] Button friendToggleButton;
+
+    private void Start()
+    {
+        if (!Account.LoggedIn)
+        {
+            friendToggleButton.interactable = false;
+            friendPanel.SetActive(false);
+            return;
+        }
+
+        GetFriends();
+    }
+    void DisplayFriends(List<FriendInfo> friendsCache)
+    {
+        foreach (FriendInfo f in friendsCache)
+        {
+            bool isFound = false;
+
+            foreach (FriendInfo g in myFriendsInScrollView)
+            {
+                if (f.FriendPlayFabId == g.FriendPlayFabId)
+                {
+                    isFound = true;
+                    break;
+                }
+            }
+
+
+
+            if (!isFound)
+            {
+                GameObject listing = Instantiate(uiFriendPrefab, friendScrollView);
+                UIFriend uifriend = listing.GetComponent<UIFriend>();
+                uifriend.Initialize(f.TitleDisplayName);
+
+            }
+
+
+        }
+        myFriendsInScrollView = friendsCache;
+    }
+
+
+
+    IEnumerator WaitForFriend()
+    {
+        yield return new WaitForSeconds(2);
+        GetFriends();
+    }
+
+    public void RunWaitFunction()
+    {
+        StartCoroutine(WaitForFriend());
+    }
+
+    void DisplayPlayFabError(PlayFabError error)
+    {
+        Debug.Log(error.GenerateErrorReport());
+    }
+
+
+    void DisplayError(string error)
+    {
+        Debug.LogError(error);
+    }
+
+
+
+    public void GetFriends()
+    {
+        PlayFabClientAPI.GetFriendsList(new GetFriendsListRequest
+        {
+            IncludeSteamFriends = false,
+            IncludeFacebookFriends = false,
+            XboxToken = null
+        }, result => {
+            //_friends = result.Friends;
+            Account.friendsList = result.Friends;
+            DisplayFriends(Account.friendsList); // triggers your UI
+        }, DisplayPlayFabError);
+    }
+
+
+
+    void AddFriend(FriendIdType idType, string friendId)
+    {
+        var request = new AddFriendRequest();
+        switch (idType)
+        {
+            case FriendIdType.PlayFabId:
+                request.FriendPlayFabId = friendId;
+                break;
+            case FriendIdType.Username:
+                request.FriendUsername = friendId;
+                break;
+            case FriendIdType.Email:
+                request.FriendEmail = friendId;
+                break;
+            case FriendIdType.DisplayName:
+                request.FriendTitleDisplayName = friendId;
+                break;
+        }
+        // Execute request and update friends when we are done
+        PlayFabClientAPI.AddFriend(request, result => {
+            Debug.Log("Friend added successfully!");
+
+            GetFriends();
+        }, DisplayPlayFabError);
+    }
+
+
+    // unlike AddFriend, RemoveFriend only takes a PlayFab ID
+    // you can get this from the FriendInfo object under FriendPlayFabId
+    void RemoveFriend(FriendInfo friendInfo)
+    {
+        PlayFabClientAPI.RemoveFriend(new RemoveFriendRequest
+        {
+            FriendPlayFabId = friendInfo.FriendPlayFabId
+        }, result => {
+            //_friends.Remove(friendInfo);
+            Account.friendsList.Remove(friendInfo);
+        }, DisplayPlayFabError);
+    }
+
+
+
+
+    private string friendSearch;
+    [SerializeField] GameObject friendPanel;
+
+    public void InputFriendUserName(string friendUserName)
+    {
+        friendSearch = friendUserName;
+    }
+
+    public void SubmitFriendRequest()
+    {
+        AddFriend(FriendIdType.Username, friendSearch);
+    }
+
+    public void ToggleFriendPanel()
+    {
+        //if (!friendPanel.activeSelf)
+        //    GetFriends();
+        friendPanel.SetActive(!friendPanel.activeSelf);
+        //friendPanel.SetActive(!friendPanel.activeInHierarchy);
+
+    }
+
+
+}
---
+++ Assets/Scripts/UIFriend.cs
@@ -0,0 +1,92 @@
+using System;
+using TMPro;
+//using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Chat;
+
+
+    public class UIFriend : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text friendNameText;
+        [SerializeField] private string friendName;
+        [SerializeField] private bool isOnline;
+        [SerializeField] private Image onlineImage;
+        [SerializeField] private Color onlineColor = new Color(0, 236, 76);
+        [SerializeField] private Color offlineColor = new Color(209, 209, 209);
+
+        public static Action<string> OnRemoveFriend = delegate { };
+        public static Action<string> OnInviteFriend = delegate { };
+        public static Action<string> OnGetCurrentStatus = delegate { };
+        public static Action OnGetRoomStatus = delegate { };
+
+        private void Awake()
+        {
+            //PhotonChatFriendController.OnStatusUpdated += HandleStatusUpdated;
+        }
+        private void OnDestroy()
+        {
+            //PhotonChatFriendController.OnStatusUpdated -= HandleStatusUpdated;
+        }
+
+        private void OnEnable()
+        {
+            if (string.IsNullOrEmpty(friendName)) return;
+            OnGetCurrentStatus?.Invoke(friendName);
+            OnGetRoomStatus?.Invoke();
+        }
+
+        //public void Initialize(FriendInfo friend)
+        //{
+        //    Debug.Log($"{friend.UserId} is online: {friend.IsOnline} ; in room: {friend.IsInRoom} ; room name: {friend.Room}");
+
+        //    SetupUI();
+        //}
+        public void Initialize(string friendName)
+        {
+            Debug.Log($"{friendName} is added");
+            this.friendName = friendName;
+
+            SetupUI();
+            OnGetCurrentStatus?.Invoke(friendName);
+            OnGetRoomStatus?.Invoke();
+        }
+
+
+        //private void HandleStatusUpdated(PhotonStatus status)
+        //{
+        //    if (string.Compare(friendName, status.PlayerName) == 0)
+        //    {
+        //        Debug.Log($"Updating status in UI for {status.PlayerName} to status {status.Status}");
+        //        SetStatus(status.Status);
+        //    }
+        //}
+
+        private void SetupUI()
+        {
+            friendNameText.SetText(friendName);
+        }
+
+        private void SetStatus(int status)
+        {
+            if (status == ChatUserStatus.Online)
+            {
+                onlineImage.color = onlineColor;
+                isOnline = true;
+                OnGetRoomStatus?.Invoke();
+            }
+            else
+            {
+                onlineImage.color = offlineColor;
+                isOnline = false;
+            }
+        }
+
+        public void RemoveFriend()
+        {
+            Debug.Log($"Clicked to remove friend {friendName}");
+            OnRemoveFriend?.Invoke(friendName);
+        }
+
+     
+    }
