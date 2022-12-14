b05194847be315a151a1b6adeaaaeceb552cbdf8
takoz53 <takoz53@users.noreply.github.com>
2022-12-15 00:35:57 +0100

    Change DateTimeConverter to use UTC

--- Assets/Scripts/Helper/DateTimeConverter.cs
+++ Assets/Scripts/Helper/DateTimeConverter.cs
@@ -1,11 +1,9 @@
 using System;
-using UnityEngine;
-
 public static class DateTimeConverter
 {
     public static int ToUnixTimeSeconds(DateTime date)
     {
-        DateTime point = new DateTime(1970, 1, 1);
+        DateTime point = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
         TimeSpan time = date.Subtract(point);
 
         return (int)time.TotalSeconds;
@@ -14,7 +12,7 @@ public static class DateTimeConverter
     public static DateTime UnixTimeStampToDateTime(double unixTime)
     {
         DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-        dateTime = dateTime.AddSeconds(unixTime).ToLocalTime();
+        dateTime = dateTime.AddSeconds(unixTime).ToUniversalTime();
         return dateTime;
     }
     public static int ToUnixTimeSeconds()
