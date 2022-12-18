483c4977d97d76d67791155bf98aeddc46de5a55
yberber <yusufberber19@gmail.com>
2022-12-18 19:56:16 +0100

    expand UIFriend Script and adapt UserInfoManager

--- Assets/Scripts/Friend/UIFriend.cs
+++ Assets/Scripts/Friend/UIFriend.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 using UnityEngine.UI;
 using Photon.Chat;
 using PlayFab.ClientModels;
-
+using PlayFab;
+using System.Net;
+using System.Collections.Generic;
 
 public class UIFriend : MonoBehaviour
 {
@@ -21,6 +23,8 @@ public class UIFriend : MonoBehaviour
 
     private FriendInfo friendInfo;
     public static PlayFabFriendManager friendManager;
+
+    public static Action<int, string, int, int, Dictionary<string, int>, List<string>, string> OpenPlayerInfoPanelForFriendAction = delegate { };
 
     private void Awake()
     {
@@ -102,6 +106,64 @@ public class UIFriend : MonoBehaviour
     private  void FriendEntryClicked()
     {
         Debug.Log($"You clicked on {friendName}");
+
+        var request = new GetPlayerProfileRequest();
+        request.PlayFabId = friendInfo.FriendPlayFabId;
+        request.ProfileConstraints = new PlayerProfileViewConstraints()
+        {
+            ShowStatistics = true
+        };
+        PlayFabClientAPI.GetPlayerProfile(request, GetProfileSuccess, error=>Debug.Log(error.GenerateErrorReport()));
     }
+
+    private void GetProfileSuccess(GetPlayerProfileResult result)
+    {
+        //Debug.Log(result);
+        //Debug.Log(result.ToJson());
+        //Debug.Log(result.ToString());
+        string userDisplayName = friendName;
+        List<string> skinIdList = new List<string>();
+        Dictionary<string, int> upgradeList = new Dictionary<string, int>();  
+        string activeSkinId = null;
+        int credits = 0;
+        int passedSeconds = -1;
+        int selectedPictureId = 0;
+
+        foreach (var stat in result.PlayerProfile.Statistics)
+        {
+            switch (stat.Name)
+            {
+                case "Credits":
+                    credits = stat.Value;
+                    break;
+                case "LeavingGameTime":
+                    if(!isOnline)
+                        passedSeconds = (int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds - stat.Value;
+                    break;
+                case "SelectedPictureId":
+                    selectedPictureId = stat.Value;
+                    break;
+                case string skin when skin.StartsWith("SKIN_"):
+                    string skinId = skin.Substring(5);
+                    if (stat.Value == 1)
+                    {
+                        skinIdList.Add(skinId);
+                    }
+                    else if (stat.Value == 2)
+                    {
+                        skinIdList.Add(skinId);
+                        activeSkinId = skinId;
+                    }
+                    break;
+                default:
+                    upgradeList.Add(stat.Name, stat.Value);
+                    break;
+            }
+        }
+
+        OpenPlayerInfoPanelForFriendAction?.Invoke(selectedPictureId, userDisplayName, passedSeconds, credits, upgradeList, skinIdList, activeSkinId);
+
+    }
+
 
 }
